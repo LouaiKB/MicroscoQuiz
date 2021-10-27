6d3744904a1f8c087235c7cbcb2dd737ecdd7699
LouaiKB <loukb1998@gmail.com>
2021-10-27 10:38:08 +0200

    update class

--- users/log_form_view.py
+++ users/log_form_view.py
@@ -1,10 +1,6 @@
 from django.contrib.messages.views import SuccessMessageMixin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView
 
 class LoginFormView(SuccessMessageMixin, LoginView):
     template_name = "users\\login.html"
     success_message = "You were successfully logged in."
-
-class LogoutFormView(SuccessMessageMixin, LogoutView):
-    template_name = "users\\logout.html"
-    # success_message = "You were successfully logged out."
