d42a5371f557e172d061839ff0e0520811282b70
LouaiKB <loukb1998@gmail.com>
2021-10-27 10:35:19 +0200

    adding admin site registrations for models

--- biologicalquizapp/admin.py
+++ biologicalquizapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
+from users.models import *
 
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Profile)
