ef36e22e9b4a3108858d99d7c8952bad6bf1b3d9
LouaiKB <loukb1998@gmail.com>
2021-10-29 17:15:57 +0200

    updating static methods

--- biologicalquizapp/models.py
+++ biologicalquizapp/models.py
@@ -1,3 +1,4 @@
+from typing import List
 from django.db import models
 from random import sample
 from random import choice
@@ -18,23 +19,33 @@ class Image(models.Model) :
     @staticmethod
     def get_random_images(field, number):
         list_of_distinct_field = [i[field] for i in list(Image.objects.order_by().values(field).distinct())]
-        rand_field = choice(list_of_distinct_field)
+        # rand_field = choice(list_of_distinct_field)
+        result_list = []
+
         if field == 'mode':
-            list_of_specific_field = list(Image.objects.filter(mode=rand_field))
+            for i in range(15):
+                rand_field = choice(list_of_distinct_field)
+                list_of_specific_field = list(Image.objects.filter(mode=rand_field))
+                result_list.append({rand_field: sample(list_of_specific_field, number)})
+
         elif field == 'components':
             # this while loop is created to prevent that we have only one record in our list
-            list_of_specific_field = [0]
-            while len(list_of_specific_field) == 1:
-                list_of_specific_field = list(Image.objects.filter(components=rand_field))
-                rand_field = choice(list_of_distinct_field)
+            for i in range(10):
+                list_of_specific_field = [0]
+                while len(list_of_specific_field) == 1:
+                    rand_field = choice(list_of_distinct_field)
+                    list_of_specific_field = list(Image.objects.filter(components=rand_field))
+                result_list.append({rand_field: sample(list_of_specific_field, number)})
+                
         elif field == 'celltype':
-            list_of_specific_field = [0]
-            while len(list_of_specific_field) == 1:
-                list_of_specific_field = list(Image.objects.filter(celltype=rand_field))
-                rand_field = choice(list_of_distinct_field)
-            
-
-        return sample(list_of_specific_field, number)
+            for i in range(10):
+                list_of_specific_field = [0]
+                while len(list_of_specific_field) == 1:
+                    rand_field = choice(list_of_distinct_field)
+                    list_of_specific_field = list(Image.objects.filter(celltype=rand_field))
+                result_list.append({rand_field: sample(list_of_specific_field, number)})
+                                
+        return result_list
 
 class Question(models.Model):
     question = models.CharField(max_length=255)
@@ -54,3 +65,9 @@ class Answer(models.Model):
 
     def __str__(self) -> str:
         return self.answer
+    
+    # a static function to generate random answers 
+    @staticmethod
+    def generate_random_answers(idd):
+        list_of_answers = [i['answer'] for i in Answer.objects.filter(question_id=idd).values()]
+        return sample(list_of_answers, 4)
