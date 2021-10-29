a4d90ecaf523ba53cb25630ab6f222222f08544e
LouaiKB <loukb1998@gmail.com>
2021-10-29 17:16:53 +0200

    updating view

--- biologicalquizapp/views.py
+++ biologicalquizapp/views.py
@@ -1,3 +1,4 @@
+from random import randint, shuffle
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -17,8 +18,62 @@ def quiz_view(request):
         'title': 'quiz page',
         'question_1': Question.objects.filter(id=1).values()[0]['question'],
         'question_2': Question.objects.filter(id=2).values()[0]['question'],
-        'answer_1': Answer.objects.filter(question_id=1).values(),
-        'answer_2': Answer.objects.filter(question_id=2).values(),
-        'images': ["data\\" + str(i.name) + ".jpg" for i in Image.get_random_images('celltype', 2)]
     }
     return render(request, 'biologicalquizapp\quiz.html', context)
+
+@login_required
+def microscopy_quiz(request):
+    images_and_answer = Image.get_random_images('mode', 3)
+    context = {}
+    context['title'] = 'microscopy quiz'
+    context['question_1'] = Question.objects.filter(id=1).values()[0]['question']
+    num = 1
+    for element in images_and_answer:
+        answers = Answer.generate_random_answers(1)
+        correct_answer = list(element.keys())[0]
+       
+        if correct_answer not in answers:
+            answers[-1] = correct_answer
+
+        shuffle(answers)
+        context['num' + str(num)] = {
+            'images': ["data\\" + str(i.name) + ".jpg" for i in element[list(element.keys())[0]]],
+            'answers': answers,
+            'correct_answer': correct_answer
+        }
+        num += 1
+    return render(request, 'biologicalquizapp\microscopy_quiz.html', context)
+ 
+@login_required
+def features_quiz(request):
+    # we will pick up random question from celltype or components
+    random_num = randint(1, 2)
+    
+    # if 1 we will choose the components questions
+    if random_num == 1:
+        images_and_answer = Image.get_random_images('components', 2)
+
+    elif random_num == 2:
+        images_and_answer = Image.get_random_images('celltype', 2)
+
+    context = {}
+    context['title'] = 'features quiz'
+    context['question_2'] = Question.objects.filter(id=2).values()[0]['question']
+    num = 1
+
+    for element in images_and_answer:
+        answers = Answer.generate_random_answers(2)
+        correct_answer = list(element.keys())[0]
+       
+        if correct_answer not in answers:
+            answers[-1] = correct_answer
+
+        shuffle(answers)
+        context['num' + str(num)] = {
+            'images': ["data\\" + str(i.name) + ".jpg" for i in element[list(element.keys())[0]]],
+            'answers': answers,
+            'correct_answer': correct_answer
+        }
+        num += 1
+    
+    return render(request, 'biologicalquizapp\\feature_quiz.html', context)
