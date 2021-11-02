0f71e94c8961cd363989037fdfdd47ce0d210d56
LouaiKB <loukb1998@gmail.com>
2021-11-02 11:52:49 +0100

    create js

--- biologicalquizapp/static/biologicalquizapp/js/script.js
+++ biologicalquizapp/static/biologicalquizapp/js/script.js
@@ -10,7 +10,12 @@ const quiz_box = document.querySelector(".quiz_box");
 const logo_paragraph_box = document.querySelector(".choose-type");
 const question_choose = document.querySelector(".question-chooser");
 const quit_btn = document.querySelector(".quit-btn");
+const next_btn = document.querySelector(".next_btn");
+const img = document.getElementById("img");
 
+next_btn.onclick = () => {
+    console.log(img);
+}
 
 // if microscopy quiz button is clicked
 // microscopy_quiz.onclick = () => {
@@ -49,14 +54,109 @@ continue_btn.onclick = () => {
     quiz_box.classList.add("activeQuiz");
 }
 
-$(document).ready(function () {
-    $.ajax({
-        type: "GET",
-        url: "{% url '/quiz/microscopy_quiz' %}",
-        data: "data",
-        dataType: "dataType",
-        success: function (response) {
-            
+const url = window.location.href;
+console.log("sdfjlsdkfj " + url);
+
+// function call_ajax() {
+    // return 
+$.ajax({
+    type: 'GET',
+    url: `${url}save`,
+    success: function (response) {
+        var data = response.data;
+        var data_len = response.data.length;
+        var image = response.data[0].images;
+        console.log('length: ', image.length);
+        const images_field = document.querySelector(".images");
+        const option_list = document.querySelector(".option_list");
+        // var next_btn = document.querySelector(".next_btn");
+        // console.log('response length ' + response.data);
+        console.log(data);
+        index = 0;
+        next_btn.onclick = () => {
+            if (index < data_len- 1) {
+                index++;
+                var num_img = image.length;
+                console.log('number images ' + num_img);
+                showQuestions(index, data, num_img);
+            } else {
+                console.log('questions completed');
+            }
         }
-    });    
+    },
+    error: function(response) {
+        console.log('wronnnnnnnnnnnnnggg!!!!')
+    }
 });
+// }
+// call_ajax();
+
+let que_count = 0;
+
+
+// $.when(call_ajax()).done(function(response){
+//     console.log(response.data.length)
+//     next_btn.onclick = () => {
+//         console.log('response length ' + response.data.length);
+//         if (que_count < response.data.length - 1) {
+//             que_count++;
+//             var num_img = response.data['images'].length;
+//             console.log('number images ' + num_img);
+//             showQuestions(que_count, response.data, num_img);
+//         } else {
+//             console.log('questions completed');
+//         }
+//     }
+// });
+
+console.log(window.location.origin);
+
+
+// getting questions and options from array
+function showQuestions(index, data, num_img) {
+    var url_origin = window.location.origin;
+    const images_field = document.querySelector(".images");
+    const option_list = document.querySelector(".option_list");
+    if(num_img == 3) {
+        let img_tag = '<img src='+ url_origin+'/static/'+data[index]['images'][0]+' alt="cell" class="img-option">' + 
+                      '<img src='+url_origin+ '/static/'+data[index]['images'][1]+' alt="cell" class="img-option">' +
+                      '<img src=' + url_origin+'/static/'+data[index]['images'][2]+' alt="cell" class="img-option">';
+        images_field.innerHTML = img_tag;
+    } else if(num_img == 2) {
+        let img_tag = '<img src=' + url_origin+'/static/'+data[index]['images'][0]+' alt="cell" class="img-option">' + 
+                      '<img src='+ url_origin+'/static/'+data[index]['images'][1]+' alt="cell" class="img-option">';
+        images_field.innerHTML = img_tag;
+    }
+    let option_tag = '<div class="option"><span>' + data[index]['answers'][0] + '</span></div>' +
+                     '<div class="option"><span>' + data[index]['answers'][1] + '</span></div>' + 
+                     '<div class="option"><span>' + data[index]['answers'][2] + '</span></div>' + 
+                     '<div class="option"><span>' + data[index]['answers'][3] + '</span></div>';
+    option_list.innerHTML = option_tag;
+}
+
+
+
+// if (index < data_len - 1) {
+//     index++;
+//     var num_img = image.length;
+//     console.log('number images ' + num_img);
+//     // showQuestions(que_count, response.data, num_img);
+//     if(num_img == 3) {
+//         let img_tag = '<img src="{% static ' + data[index]['images'][0] + ' %}" alt="cell" class="img-option">' + 
+//                       '<img src="{% static ' + data[index]['images'][1] + ' %}" alt="cell" class="img-option">' +
+//                       '<img src="{% static ' + data[index]['images'][2] + ' %}" alt="cell" class="img-option">';
+//         images_field.innerHTML = img_tag;
+//     } else if(num_img == 2) {
+//         let img_tag = '<img src="{% static ' + data[index]['images'][0] + ' %}" alt="cell" class="img-option">' + 
+//                       '<img src="{% static ' + data[index]['images'][1] + ' %}" alt="cell" class="img-option">';
+//         images_field.innerHTML = img_tag;
+//     }
+//     let option_tag = '<div class="option"><span>' + data[index]['answer'][0] + '</span></div>' +
+//                      '<div class="option"><span>' + data[index]['answer'][1] + '</span></div>' + 
+//                      '<div class="option"><span>' + data[index]['answer'][2] + '</span></div>' + 
+//                      '<div class="option"><span>' + data[index]['answer'][3] + '</span></div>';
+//     option_list.innerHTML = option_tag;
+// } else {
+//     console.log('questions completed');
+// }
+// }
--- biologicalquizapp/views.py
+++ biologicalquizapp/views.py
@@ -1,9 +1,10 @@
 from random import randint, shuffle
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Answer, Image, Question
+from django.views import View
 
 # Create your views here.
 def home(request):
@@ -21,12 +22,39 @@ def quiz_view(request):
     }
     return render(request, 'biologicalquizapp\quiz.html', context)
 
+# @login_required
+class MicroscopyQuizView(View):
+    template_name = 'biologicalquizapp\quiz.html'
+    def get(self, request, *args, **kwargs):
+        if not request.is_ajax():
+            images_and_answer = Image.get_random_images('mode', 3)
+            context = {}
+            context['title'] = 'microscopy quiz'
+            context['question_1'] = Question.objects.filter(id=1).values()[0]['question']
+            num = 1
+            for element in images_and_answer:
+                answers = Answer.generate_random_answers(1)
+                correct_answer = list(element.keys())[0]
+            
+                if correct_answer not in answers:
+                    answers[-1] = correct_answer
+
+                shuffle(answers)
+                context['num' + str(num)] = {
+                    'images': ["data\\" + str(i.name) + ".jpg" for i in element[list(element.keys())[0]]],
+                    'answers': answers,
+                    'correct_answer': correct_answer
+                }
+                num += 1    
+            return JsonResponse(context)
+        return HttpResponse('wrong request')
+
+
 @login_required
-def microscopy_quiz(request):
+def microscopy_quiz_data(request):
     images_and_answer = Image.get_random_images('mode', 3)
     context = {}
-    context['title'] = 'microscopy quiz'
-    context['question_1'] = Question.objects.filter(id=1).values()[0]['question']
+    context['data'] = []
     num = 1
     for element in images_and_answer:
         answers = Answer.generate_random_answers(1)
@@ -36,12 +64,46 @@ def microscopy_quiz(request):
             answers[-1] = correct_answer
 
         shuffle(answers)
-        context['num' + str(num)] = {
+        context['data'].append({
+            'num': num,
             'images': ["data\\" + str(i.name) + ".jpg" for i in element[list(element.keys())[0]]],
             'answers': answers,
             'correct_answer': correct_answer
-        }
+        })
         num += 1
+    return JsonResponse({'data': context['data']})
+
+@login_required
+def microscopy_quiz(request):
+    images_and_answer = Image.get_random_images('mode', 3)
+    context = {}
+    context['title'] = 'microscopy quiz'
+    context['question_1'] = Question.objects.filter(id=1).values()[0]['question']
+    context['images'] = ["data\\" + str(i.name) + ".jpg" for i in images_and_answer[0][list(images_and_answer[0].keys())[0]]]
+    answers = Answer.generate_random_answers(1)
+    correct_answer = list(images_and_answer[0].keys())[0]
+    if correct_answer not in answers:
+        answers[-1] = correct_answer
+    context['answers'] = answers
+    context['correct_answer'] = correct_answer
+    # context['data'] = []
+    # num = 1
+    # for element in images_and_answer:
+    #     answers = Answer.generate_random_answers(1)
+    #     correct_answer = list(element.keys())[0]
+       
+    #     if correct_answer not in answers:
+    #         answers[-1] = correct_answer
+
+    #     shuffle(answers)
+    #     context['data'].append({
+    #         'num': num,
+    #         'images': ["data\\" + str(i.name) + ".jpg" for i in element[list(element.keys())[0]]],
+    #         'answers': answers,
+    #         'correct_answer': correct_answer
+    #     })
+    #     num += 1
+    # return JsonResponse(context)
     return render(request, 'biologicalquizapp\microscopy_quiz.html', context)
  
 @login_required
@@ -59,6 +121,7 @@ def features_quiz(request):
     context = {}
     context['title'] = 'features quiz'
     context['question_2'] = Question.objects.filter(id=2).values()[0]['question']
+    context['data'] = []
     num = 1
 
     for element in images_and_answer:
@@ -69,11 +132,12 @@ def features_quiz(request):
             answers[-1] = correct_answer
 
         shuffle(answers)
-        context['num' + str(num)] = {
+        context['data'].append({
+            'num': num,
             'images': ["data\\" + str(i.name) + ".jpg" for i in element[list(element.keys())[0]]],
             'answers': answers,
             'correct_answer': correct_answer
-        }
+        })
         num += 1
     
     return render(request, 'biologicalquizapp\\feature_quiz.html', context)
