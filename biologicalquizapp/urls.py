3c9df347da38d1149172fd8935ea5bd47fe51e40
LouaiKB <loukb1998@gmail.com>
2021-11-03 14:19:22 +0100

    update views and urls

--- biologicalquizapp/urls.py
+++ biologicalquizapp/urls.py
@@ -6,8 +6,6 @@ urlpatterns = [
     path('', views.home),
     path('home/', views.home, name='home'),
     path('quizapp/', views.quiz_view, name='quiz-view'),
-    path('quizapp/microscopy_quiz/save', views.microscopy_quiz_data, name='microscopy-data'),
-    path('quizapp/features_quiz/save', views.feature_quiz_data, name='feature-data'),
     path('quizapp/microscopy_quiz/', views.microscopy_quiz, name='microscopy-quiz'),
     path('quizapp/features_quiz/', views.features_quiz, name='features-quiz')
 ]
--- biologicalquizapp/views.py
+++ biologicalquizapp/views.py
@@ -23,9 +23,11 @@ def quiz_view(request):
     return render(request, 'biologicalquizapp\quiz.html', context)
 
 @login_required
-def microscopy_quiz_data(request):
+def microscopy_quiz(request):
     images_and_answer = Image.get_random_images('mode', 3)
     context = {}
+    context['title'] = 'microscopy quiz'
+    context['question_1'] = Question.objects.filter(id=1).values()[0]['question']
     context['data'] = []
     num = 1
     for element in images_and_answer:
@@ -43,55 +45,6 @@ def microscopy_quiz_data(request):
             'correct_answer': correct_answer
         })
         num += 1
-    return JsonResponse({'data': context['data']})
-
-@login_required
-def feature_quiz_data(request):
-    # we will pick up random question from celltype or components
-    random_num = randint(1, 2)
-    
-    # if 1 we will choose the components questions
-    if random_num == 1:
-        images_and_answer = Image.get_random_images('components', 2)
-
-    elif random_num == 2:
-        images_and_answer = Image.get_random_images('celltype', 2)
-
-    context = {}
-    context['data'] = []
-    num = 1
-    for element in images_and_answer:
-        answers = Answer.generate_random_answers(2)
-        correct_answer = list(element.keys())[0]
-       
-        if correct_answer not in answers:
-            answers[-1] = correct_answer
-
-        shuffle(answers)
-        context['data'].append({
-            'num': num,
-            'images': ["data\\" + str(i.name) + ".jpg" for i in element[list(element.keys())[0]]],
-            'answers': answers,
-            'correct_answer': correct_answer
-        })
-        num += 1
-    return JsonResponse({'data': context['data']})
-
-
-@login_required
-def microscopy_quiz(request):
-    images_and_answer = Image.get_random_images('mode', 3)
-    context = {}
-    context['title'] = 'microscopy quiz'
-    context['question_1'] = Question.objects.filter(id=1).values()[0]['question']
-    context['images'] = ["data\\" + str(i.name) + ".jpg" for i in images_and_answer[0][list(images_and_answer[0].keys())[0]]]
-    answers = Answer.generate_random_answers(1)
-    correct_answer = list(images_and_answer[0].keys())[0]
-    if correct_answer not in answers:
-        answers[-1] = correct_answer
-    shuffle(answers)
-    context['answers'] = answers
-    context['correct_answer'] = correct_answer
     return render(request, 'biologicalquizapp\microscopy_quiz.html', context)
  
 @login_required
@@ -109,12 +62,22 @@ def features_quiz(request):
     context = {}
     context['title'] = 'features quiz'
     context['question_2'] = Question.objects.filter(id=2).values()[0]['question']
-    context['images'] = ["data\\" + str(i.name) + ".jpg" for i in images_and_answer[0][list(images_and_answer[0].keys())[0]]]
-    answers = Answer.generate_random_answers(1)
-    correct_answer = list(images_and_answer[0].keys())[0]
-    if correct_answer not in answers:
-        answers[-1] = correct_answer
-    shuffle(answers)
-    context['answers'] = answers
-    context['correct_answer'] = correct_answer
+    context['data'] = []
+    num = 1
+    for element in images_and_answer:
+        answers = Answer.generate_random_answers(2)
+        correct_answer = list(element.keys())[0]
+       
+        if correct_answer not in answers:
+            answers[-1] = correct_answer
+
+        shuffle(answers)
+        context['data'].append({
+            'num': num,
+            'images': ["data\\" + str(i.name) + ".jpg" for i in element[list(element.keys())[0]]],
+            'answers': answers,
+            'correct_answer': correct_answer
+        })
+        num += 1
+
     return render(request, 'biologicalquizapp\\feature_quiz.html', context)
