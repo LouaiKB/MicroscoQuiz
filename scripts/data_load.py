6a1c8f3c28fa722cb411036cee19b9b18a6aef1b
LouaiKB <loukb1998@gmail.com>
2021-11-20 16:32:31 +0100

    update script

--- scripts/data_load.py
+++ scripts/data_load.py
@@ -13,25 +13,9 @@ def run():
     dataReaderI = csv.reader(open(csv_images, encoding="utf8"), delimiter='|', quotechar='"')
 
     # Parse files
-    for row in dataReaderQ:
-        question = Question()
-        question.question = row[1]
-        question.type = row[2]
-        question.imagefield = row[3]
-        question.points = row[4]
-        question.n_answer = row[5]
-        question.n_image = row[6]
-        question.save()
-
-    for row in dataReaderA:
-        answer=Answer()
-        answer.question_id = row[1]
-        answer.answer = row[2]
-        answer.definition = row[3]
-        answer.save()
-
     for row in dataReaderI:
         image=Image()
+        question = Question()
         image.name = row[1]
         image.description = row[2]
         image.mode = row[3]
@@ -40,3 +24,37 @@ def run():
         image.doi = row[6]
         image.organism = row[7]
         image.save()
+
+    for row in dataReaderQ:
+        question = Question()
+        answer = Answer()
+        
+        question.question = row[1]
+
+        if row[1] == '1':
+            image = Image.objects.values('mode').distinct()
+            question.imagefield.add(image)
+
+        elif row[1] == '2':
+            image = Image.objects.values('components').distinct()
+            question.imagefield.add(image)
+
+        question.type = row[2]
+        question.points = row[4]
+        question.n_answer = row[5]
+        question.n_image = row[6]
+        question.save()
+
+    for row in dataReaderA:
+        answer=Answer()
+
+        if row[1] == '1':
+            question = Question.objects.get(id=1)
+
+        elif row[1] == '2':
+            question = Question.objects.get(id=2)
+
+        answer.question_id = question
+        answer.answer = row[2]
+        answer.definition = row[3]
+        answer.save()
